9805f787fa5920d509e320e4110b5b5d988318cb
Ava Pek <exodrifter@gmail.com>
2017-10-08 13:06:13 -0500

    Initial market

---
+++ Assets/Scripts/Market.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Market : MonoBehaviour
+{
+	public Pool pool;
+
+	public void Update()
+	{
+		pool.DespawnAll();
+
+		for (int i = 0; i < 3; ++i)
+		{
+			var go = pool.Spawn();
+			go.GetComponent<Anchor>().offset = new Vector3(1.2f, -1 - i * 2, 1);
+		}
+
+		pool.UpdateActiveState();
+	}
+}
