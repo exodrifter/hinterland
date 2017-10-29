1edffcbee0c1b03251916bed0ecc0e8c9e1ec9e0
Ava Pek <exodrifter@gmail.com>
2017-10-29 14:28:13 -0500

    Add PlayerInfo scripts

---
+++ Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInfo : MonoBehaviour
+{
+	[SerializeField]
+	private GameManager manager;
+
+	[SerializeField]
+	private int playerIndex;
+
+	public Text moneyText;
+	public Text populationText;
+
+	void Update ()
+	{
+		var player = manager.Game.players[playerIndex];
+
+		var incomeSign = player.income < 0 ? "-" : "+";
+		var reputationSign = player.reputation < 0 ? "-" : "+";
+		moneyText.text = string.Format("${0} ({1}{2})",
+			player.money, incomeSign, Mathf.Abs(player.income)
+		);
+		populationText.text = string.Format("{0} ({1}{2})",
+			player.population, reputationSign, Mathf.Abs(player.reputation)
+		);
+	}
+}
