780eb6b2c75f12fa5dc0eb90ff9d371cf17d09c0
Axel Ismirlian <axel.ismirlian@gmail.com>
2017-11-26 15:19:05 -0600

    Add camera controls

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+	public float cameraSpeed;
+
+	// Update is called once per frame
+	void Update ()
+	{
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * cameraSpeed;
+		var y = Input.GetAxis("Vertical") * Time.deltaTime * cameraSpeed;
+
+		transform.Translate(x, y, 0);
+	}
+}
