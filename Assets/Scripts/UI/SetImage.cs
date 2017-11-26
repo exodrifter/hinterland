17059a7eaaea927213d3b10ab344f41d1edb6c43
Ava Pek <exodrifter@gmail.com>
2017-11-26 12:46:01 -0600

    Add initial UI mask

---
+++ Assets/Scripts/UI/SetImage.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SetImage : MonoBehaviour
+{
+	void Awake()
+	{
+		ImageHelper.LoadImage("suburbia/community-park.png", (tex) => {
+			var sprite = Sprite.Create((Texture2D)tex, new Rect(0, 0, tex.width, tex.height), Vector2.one * 0.5f);
+			GetComponent<Image>().sprite = sprite;
+		});
+	}
+}
